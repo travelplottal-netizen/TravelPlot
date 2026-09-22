616b2e052398457cb3f0118b0dfc349fc1ce081d
travelplottal-netizen <travelplottal@gmail.com>
2026-09-22 13:29:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("TravelPlot website loaded successfully!");
+const menuButton = document.getElementById("menuButton");
+const nav = document.getElementById("nav");
+
+menuButton.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+const navLinks = document.querySelectorAll("#nav a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+});
